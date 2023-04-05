f4097b5b2f15c1ba6260c03885f1f320b473d1c3
devbloo <zpswl45@naver.com>
2023-04-05 11:20:23 +0900

    init: init project

---
+++ adapters/in/web/src/main/java/com/web/novel/WebNovelServiceApplication.java
@@ -0,0 +1,11 @@
+package com.web.novel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebNovelServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebNovelServiceApplication.class, args);
+    }
+}
